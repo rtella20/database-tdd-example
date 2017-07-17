7c7c3297f0dcd4c63326661a57a0c3cf6a01ac48
James Byars <jamesbyars@g.harvard.edu>
2017-07-17 16:18:17 -0500

    Setup JOOq and ignore target/ directory

--- src/main/java/com/ernesttech/example/dbtdd/DatabaseTddExampleApplication.java
+++ src/main/java/com/ernesttech/example/dbtdd/DatabaseTddExampleApplication.java
@@ -1,6 +1,10 @@
 package com.ernesttech.example.dbtdd;
 
 import com.jolbox.bonecp.BoneCPDataSource;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DataSourceConnectionProvider;
+import org.jooq.impl.DefaultConfiguration;
+import org.jooq.impl.DefaultDSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -8,6 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
 
 import javax.sql.DataSource;
 
@@ -41,5 +48,50 @@ public class DatabaseTddExampleApplication {
         dataSource.setPassword(env.getRequiredProperty(PROPERTY_NAME_DB_PASSWORD));
 
         return dataSource;
+    }
+
+    @Bean(name = "defConfiguration")
+    public DefaultConfiguration configuration() {
+        DefaultConfiguration jooqConfiguration = new DefaultConfiguration();
+
+        jooqConfiguration.set(connectionProvider());
+//        jooqConfiguration.set(new DefaultExecuteListenerProvider(
+//                jooqToSpringExceptionTransformer()
+//        ));
+
+        SQLDialect dialect = SQLDialect.valueOf(SQLDialect.POSTGRES.toString());
+        jooqConfiguration.set(dialect);
+
+        return jooqConfiguration;
+    }
+
+    @Bean(name = "lazyConnDataSource")
+    public LazyConnectionDataSourceProxy lazyConnectionDataSource() {
+        return new LazyConnectionDataSourceProxy(dataSource());
+    }
+
+    @Bean(name = "transAwareDataSource")
+    public TransactionAwareDataSourceProxy transactionAwareDataSource() {
+        return new TransactionAwareDataSourceProxy(lazyConnectionDataSource());
+    }
+
+    @Bean(name = "transManager")
+    public DataSourceTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(lazyConnectionDataSource());
+    }
+
+    @Bean(name = "connectionProvider")
+    public DataSourceConnectionProvider connectionProvider() {
+        return new DataSourceConnectionProvider(transactionAwareDataSource());
+    }
+
+//    @Bean(name = "jooqExceptionTransformer")
+//    public JOOQToSpringExceptionTransformer jooqToSpringExceptionTransformer() {
+//        return new JOOQToSpringExceptionTransformer();
+//    }
+
+    @Bean(name = "defDSLContext")
+    public DefaultDSLContext dsl() {
+        return new DefaultDSLContext(configuration());
     }
 }
