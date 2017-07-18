20e04ee7a07a2f19b586719d7e52e37d8c5a8f0b
James Byars <jamesbyars@g.harvard.edu>
2017-07-17 19:02:40 -0500

    Now have a working example

--- src/main/java/com/ernesttech/example/dbtdd/managers/TestQueryManager.java
+++ src/main/java/com/ernesttech/example/dbtdd/managers/TestQueryManager.java
@@ -6,6 +6,8 @@ import org.jooq.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public class TestQueryManager {
--- src/test/java/com/ernesttech/example/dbtdd/TestRunner.java
+++ src/test/java/com/ernesttech/example/dbtdd/TestRunner.java
@@ -1,7 +1,10 @@
 package com.ernesttech.example.dbtdd;
 
 import com.ernesttech.example.dbtdd.domain.jooq.generated.tables.records.TestsRecord;
+import com.ernesttech.example.dbtdd.managers.TestQueryManager;
 import com.ernesttech.example.dbtdd.managers.TestsManager;
+import org.jooq.Record;
+import org.jooq.Result;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,12 +15,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TestRunner {
 
     @Autowired
     private TestsManager testManager;
+
+    @Autowired
+    private TestQueryManager testQueryManager;
 
     private List<TestsRecord> testCases;
 
@@ -36,9 +45,37 @@ public class TestRunner {
     public void run() {
 
         for (TestsRecord testsRecord : testCases) {
+            Result<Record> results = null;
+
+            try {
+                results = testQueryManager.executeQuery(testsRecord.getTest());
+
+                String expected = String.valueOf(testsRecord.getExpected());
+
+                String result = String.valueOf(results.getValues(0).get(0));
+
+                assertThat(result, is(expected));
+
+            } catch (Exception e) {
+                System.out.println(buildFailureMessage(results, testsRecord, e));
+            }
 
         }
 
+    }
+
+    private String buildFailureMessage(final List<?> record, final Record expected, final Exception exception) {
+        return new StringBuilder()
+                .append("ERROR - ")
+                .append("Got: \n")
+                .append(record)
+                .append("\n")
+                .append("Expected: \n")
+                .append(expected)
+                .append("\n")
+                .append("Exception: ")
+                .append(exception)
+                .toString();
     }
 
 }
