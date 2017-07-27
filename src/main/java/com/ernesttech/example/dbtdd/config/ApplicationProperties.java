697fa8fb95444c76914d09b5c89de96ac716c0f5
James Byars <jamesbyars@g.harvard.edu>
2017-07-27 08:45:53 -0400

    Factor application properties into a pojo

---
+++ src/main/java/com/ernesttech/example/dbtdd/config/ApplicationProperties.java
@@ -0,0 +1,54 @@
+package com.ernesttech.example.dbtdd.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:application.properties")
+public class ApplicationProperties {
+
+    @Value("${db.url}")
+    private String dbUrl;
+
+    @Value("${db.username}")
+    private String dbUsername;
+
+    @Value("${db.password}")
+    private String dbPassword;
+
+    @Value("${db.driver}")
+    private String dbDriver;
+
+    public String getDbUrl() {
+        return dbUrl;
+    }
+
+    public void setDbUrl(String dbUrl) {
+        this.dbUrl = dbUrl;
+    }
+
+    public String getDbUsername() {
+        return dbUsername;
+    }
+
+    public void setDbUsername(String dbUsername) {
+        this.dbUsername = dbUsername;
+    }
+
+    public String getDbPassword() {
+        return dbPassword;
+    }
+
+    public void setDbPassword(String dbPassword) {
+        this.dbPassword = dbPassword;
+    }
+
+    public String getDbDriver() {
+        return dbDriver;
+    }
+
+    public void setDbDriver(String dbDriver) {
+        this.dbDriver = dbDriver;
+    }
+}
--- src/main/java/com/ernesttech/example/dbtdd/config/DbTddConfiguration.java
+++ src/main/java/com/ernesttech/example/dbtdd/config/DbTddConfiguration.java
@@ -1,4 +1,4 @@
-package com.ernesttech.example.dbtdd;
+package com.ernesttech.example.dbtdd.config;
 
 import com.jolbox.bonecp.BoneCPDataSource;
 import org.jooq.SQLDialect;
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
 import org.springframework.jdbc.datasource.TransactionAwareDataSourceProxy;
@@ -20,22 +19,18 @@ import javax.sql.DataSource;
 public class DbTddConfiguration {
 
     @Autowired
-    private Environment env;
+    private ApplicationProperties applicationProperties;
 
-    private static final String PROPERTY_NAME_DB_DRIVER = "db.driver";
-    private static final String PROPERTY_NAME_DB_PASSWORD = "db.password";
-    private static final String PROPERTY_NAME_DB_URL = "db.url";
-    private static final String PROPERTY_NAME_DB_USERNAME = "db.username";
 
     @Bean(destroyMethod = "close", name = "dataSource")
     @Primary
     public DataSource dataSource() {
         BoneCPDataSource dataSource = new BoneCPDataSource();
 
-        dataSource.setDriverClass(env.getRequiredProperty(PROPERTY_NAME_DB_DRIVER));
-        dataSource.setJdbcUrl(env.getRequiredProperty(PROPERTY_NAME_DB_URL));
-        dataSource.setUsername(env.getRequiredProperty(PROPERTY_NAME_DB_USERNAME));
-        dataSource.setPassword(env.getRequiredProperty(PROPERTY_NAME_DB_PASSWORD));
+        dataSource.setDriverClass(applicationProperties.getDbDriver());
+        dataSource.setJdbcUrl(applicationProperties.getDbUrl());
+        dataSource.setUsername(applicationProperties.getDbUsername());
+        dataSource.setPassword(applicationProperties.getDbPassword());
 
         return dataSource;
     }
